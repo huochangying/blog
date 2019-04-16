9f7f3d247be0000de29c56315ede2346b3e6ce76
chay <chay>
2019-04-16 17:34:42 +0800

    'home'

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd'
 import styles from './index.scss';
+import TitleImg from '../assets/img/cat.jpg';
+import HeaderImg from '../assets/img/header.png';
 
 const {
     Header, Footer, Content,
@@ -10,8 +12,15 @@ class LayOut extends Component {
     render() {
         return (
             <Layout>
-                <Header >Header</Header>
-                <Content className={styles.height}>
+                <Header className={styles.header}>
+                    <div className={styles.singnature}>
+                    <img src={HeaderImg} alt=""/>
+                    </div>
+                    <div className={styles['profile-img']}>
+                        <img src={TitleImg} alt="" />
+                    </div>
+                </Header>
+                <Content>
                     Content
                 </Content>
                 <Footer>Footer</Footer>
